8528d718c1607e19c54f4792feb958d818798565
Mateito <mateolucascf@gmail.com>
2023-11-29 22:10:33 -0500

    affichage départ
    l'affichage du départ, avec les 4 trous et les cartes mélangées

--- documents/documents/tp2.py
+++ documents/documents/tp2.py
@@ -2,30 +2,60 @@
 # tel qu'indiqué dans la description du TP2.  Le code ici correspond
 # à l'exemple donné dans la description.
 
-def init():
+def brasser_cartes():
+    pass
 
-    # changer le contenu HTML de l'élément racine
+def melanger(paquet):
+    for i in range(51, 0, -1):
+        index_aleatoire = randint(0, 51)
+        temporaire = paquet[i]
+
+        paquet[i] = paquet[index_aleatoire]
+        paquet[index_aleatoire] = temporaire
+    return paquet
+
+def init():
+    liste_cartes = [
+        "2C.svg", "2D.svg", "2H.svg", "2S.svg", 
+        "3C.svg", "3D.svg", "3H.svg", "3S.svg", 
+        "4C.svg", "4D.svg", "4H.svg", "4S.svg", 
+        "5C.svg", "5D.svg", "5H.svg", "5S.svg", 
+        "6C.svg", "6D.svg", "6H.svg", "6S.svg", 
+        "7C.svg", "7D.svg", "7H.svg", "7S.svg", 
+        "8C.svg", "8D.svg", "8H.svg", "8S.svg", 
+        "9C.svg", "9D.svg", "9H.svg", "9S.svg", 
+        "10C.svg", "10D.svg", "10H.svg", "10S.svg", 
+        "JC.svg", "JD.svg", "JH.svg", "JS.svg", 
+        "QC.svg", "QD.svg", "QH.svg", "QS.svg", 
+        "KC.svg", "KD.svg", "KH.svg", "KS.svg", 
+        "empty.svg", "empty.svg", "empty.svg", "empty.svg"
+    ]
+
+    liste_cartes = melanger(liste_cartes)
+    
+
+    contenu_html = (
+    "<style>"
+    "  #jeu table { float:none; }"
+    "  #jeu table td { border:0; padding:1px 2px; height:auto; width:auto; }"
+    "  #jeu table td img { height:140px; }"
+    "</style>"
+    "<div id='jeu'>"
+    "  <table>"
+    )
+
+    index = 0
+    for i in range(4):
+        contenu_html += "<tr>"
+        for j in range(13):
+            contenu_html += "<td id='case" + str(index) + "'><img src='cards/" + liste_cartes[index] + "'></td>"
+            index += 1
+        contenu_html += "</tr>"
+
+
     racine = document.querySelector("#cb-body")
-    racine.innerHTML = """
-      <style>
-        #jeu table { float:none; }
-        #jeu table td { border:0; padding:1px 2px; height:auto; width:auto; }
-        #jeu table td img { height:140px; }
-      </style>
-      <div id="jeu">
-        <table>
-          <tr>
-            <td id="case0"><img src="cards/2S.svg"></td>
-            <td id="case1"><img src="cards/QH.svg"></td>
-          </tr>
-          <tr>
-            <td id="case2"><img src="cards/JC.svg"></td>
-            <td id="case3"><img src="cards/10D.svg"></td>
-          </tr>
-        </table>
-      </div>
-  """
-    # changer la couleur de fond de la case 0
+    racine.innerHTML = contenu_html
+
+
     case0 = document.querySelector("#case0")
     case0.setAttribute("style", "background-color: lime")
-    
